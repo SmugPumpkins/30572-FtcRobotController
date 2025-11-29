11cccdaaf270a01e73adc3d2ae0f4cae9f1fc0fc
STEM-Forsyth <nmf.forsyth@gmail.com>
2025-11-29 15:13:12 -0700

    Disable MenuDemo opmode and clean up ArcadeDrivetrain
    Added the @Disabled annotation to MenuDemo to prevent it from appearing in the driver station. Also removed an unnecessary blank line in ArcadeDrivetrain for code cleanliness.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MenuDemo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MenuDemo.java
@@ -1,9 +1,11 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.utils.Menu;
 
+@Disabled
 @TeleOp(name = "MenuDemo")
 public class MenuDemo extends OpMode {
 
