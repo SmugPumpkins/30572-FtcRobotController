78edec658ce6e752235621814966f314fb1935ac
borgle-johnson <theohutchinson6@gmail.com>
2025-11-29 15:13:42 -0600

    Started implementing state machine for launcher

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/launcherTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/launcherTest.java
@@ -1,20 +1,25 @@
 package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import static org.firstinspires.ftc.teamcode.utils.Constants.*;
 import org.firstinspires.ftc.teamcode.mechanisms.Flywheel;
 @TeleOp
+@Disabled
 public class launcherTest extends OpMode {
     Flywheel flywheel = null;
+    Launch_v1 launcher= null;
     @Override
     public void init() {
         flywheel = new Flywheel(hardwareMap, REVERSE);
+        launcher = new Launch_v1(hardwareMap);
     }
 
     @Override
     public void loop() {
         flywheel.run();
         flywheel.turnMotorOff(gamepad1.b);
-        flywheel.turnMotorOn(gamepad1.a);
+        flywheel.turnMotorOn(gamepad1.y);
+        launcher.run(gamepad1.left_bumper);
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/Hood.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode.mechanisms;
+// To do:
+// Find angle for home command
+public class Hood {
+    public void home(){
+
+    }
+    public void angle(){
+
+    }
+    public boolean is_in_pos(){
+        return true;
+    }
+}
