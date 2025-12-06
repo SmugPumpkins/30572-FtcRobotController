9d60bb06a38889b926ca70058d9d6031a67b6868
borgle-johnson <theohutchinson6@gmail.com>
2025-12-06 14:37:22 -0600

    Finished nav code and added test class, cleaned up launch code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Navigate_v1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Navigate_v1.java
@@ -8,16 +8,48 @@ import org.firstinspires.ftc.teamcode.mechanisms.MecanumDriveTrain;
 
 public class Navigate_v1 {
     MecanumDriveTrain drivetrain = null;
+    private double target_x = 0;
+    private double target_y = 0;
+    private int x_speed = 0;
+    private int y_speed = 0;
     private CoordinateSystem coords = null;
     private HardwareMap hardware_map;
+    public boolean is_at_target(){
+        if (coords.x == target_x){
+            return true;
+        } else {
+            return false;
+        }
+    }
     public void init(){
         coords = new CoordinateSystem(hardware_map);
         drivetrain = new MecanumDriveTrain(hardware_map);
         drivetrain.init(FORWARD, FORWARD, FORWARD, FORWARD);
     }
+    public void setTarget_y(double input_target_y){
+        target_y = input_target_y;
+    }
+    public void setTarget_x(double input_target_x){
+        target_x = input_target_x;
+    }
     public Navigate_v1(HardwareMap input_hardware_map) {
         hardware_map = input_hardware_map;
     }
     public void run(){
+        if (coords.x > target_x){
+            int x_speed = -1;
+        } else if (coords.x < target_x){
+            int x_speed = 1;
+        } else{
+            int x_speed = 0;
+        }
+        if (coords.y > target_y){
+            int y_speed = -1;
+        } else if (coords.y < target_y){
+            int y_speed = 1;
+        } else {
+            int y_speed = 0;
+        }
+        drivetrain.drive(y_speed, x_speed, 0);
     }
 }
