e668e60ef74822bcd249f0ba4298a8df5f8d34ae
borgle-johnson <theohutchinson6@gmail.com>
2025-12-11 17:53:48 -0600

    i dont really know what i did but it works now

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OdometryTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OdometryTest.java
@@ -1,14 +1,16 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp
-public class OdometryTest {
+public class OdometryTest extends OpMode {
     private Navigate_v2 navigate = null;
     public void init(){
         navigate = new Navigate_v2();
     }
-    public void run(){
+    @Override
+    public void loop() {
         navigate.setTarget_x(0);
         navigate.setTarget_y(0);
     }
