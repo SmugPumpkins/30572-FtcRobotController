2c8202a49591ed250fda86cca3368c77c2c2d279
borgle-johnson <theohutchinson6@gmail.com>
2025-12-11 15:45:42 -0600

    Stabilized navigation code (still WIP)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Navigate_v1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Navigate_v1.java
@@ -1,20 +1,34 @@
 package org.firstinspires.ftc.teamcode;
 
+import static org.firstinspires.ftc.teamcode.utils.Config.PINPOINT;
 import static org.firstinspires.ftc.teamcode.utils.Constants.FORWARD;
 
 import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
 import org.firstinspires.ftc.teamcode.mechanisms.MecanumDriveTrain;
 
 public class Navigate_v1 {
+    private GoBildaPinpointDriver pinpoint;
     MecanumDriveTrain drivetrain = null;
     private double target_x = 0;
     private double target_y = 0;
     private int x_speed = 0;
     private int y_speed = 0;
+    private double x_offset = 0;
+    private double y_offset = 0;
+    private GoBildaPinpointDriver.EncoderDirection x_direction = GoBildaPinpointDriver.EncoderDirection.FORWARD;
+    private GoBildaPinpointDriver.EncoderDirection y_direction = GoBildaPinpointDriver.EncoderDirection.FORWARD;
     private CoordinateSystem coords = null;
-    private HardwareMap hardware_map;
+    public void init(HardwareMap hardwareMap) {
+        pinpoint = hardwareMap.get(GoBildaPinpointDriver.class, PINPOINT);
+        configurePinpoint();
+        resetPinpointPosition();
+    }
+
     public boolean is_at_target(){
         if (coords.x == target_x){
             return true;
@@ -22,20 +36,16 @@ public class Navigate_v1 {
             return false;
         }
     }
-    public void setEncoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods goBILDA_4_BAR_POD)
-    public void init(){
-        coords = new CoordinateSystem(hardware_map);
-        drivetrain = new MecanumDriveTrain(hardware_map);
-        drivetrain.init(FORWARD, FORWARD, FORWARD, FORWARD);
+    public void setEncoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods goBILDA_4_BAR_POD){
     }
-    public void setTarget_y(double input_target_y){
-        target_y = input_target_y;
+    public void resetPinpointPosition(){
+        pinpoint.setPosition(new Pose2D(DistanceUnit.INCH, 0, 0, AngleUnit.DEGREES, 0));
     }
-    public void setTarget_x(double input_target_x){
-        target_x = input_target_x;
-    }
-    public Navigate_v1(HardwareMap input_hardware_map) {
-        hardware_map = input_hardware_map;
+    private void configurePinpoint(){
+        pinpoint.setOffsets(x_offset, y_offset, DistanceUnit.MM); //these are tuned for 3110-0002-0001 Product Insight #1
+        pinpoint.setEncoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD);
+        pinpoint.setEncoderDirections(x_direction, y_direction);
+        pinpoint.resetPosAndIMU();
     }
     public void run(){
         if (coords.x > target_x){
