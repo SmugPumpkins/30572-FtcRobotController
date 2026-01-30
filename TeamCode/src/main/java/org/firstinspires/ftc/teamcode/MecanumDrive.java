77795f761c3ac1cc2626bc06bc7f413019a8ebd8
borgle-johnson <theohutchinson6@gmail.com>
2026-01-30 17:11:28 -0600

    configured inPerTick variable for RoadRunner

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -63,7 +63,7 @@ public final class MecanumDrive {
                 RevHubOrientationOnRobot.UsbFacingDirection.FORWARD;
 
         // drive model parameters
-        public double inPerTick = 1;
+        public double inPerTick = 207.85;
         public double lateralInPerTick = inPerTick;
         public double trackWidthTicks = 0;
 
