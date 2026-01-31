bee70d44eff508147209a219ada75cb54640545b
borgle-johnson <theohutchinson6@gmail.com>
2026-01-30 18:36:34 -0600

    added auto opmodes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto1.java
@@ -0,0 +1,67 @@
+package org.firstinspires.ftc.teamcode;
+
+import static java.lang.Thread.sleep;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.mechanisms.MecanumDriveTrain;
+
+@Autonomous(name = "Strafe off line", group= "Blue Auto")
+public class Auto1 extends OpMode {
+    int step1 = 2000;
+    private MecanumDriveTrain drive;
+    public void forward(long time) throws InterruptedException {
+        drive.drive(1, 0, 0);
+        try {
+            sleep(time);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        drive.drive(0, 0, 0);
+    }
+    public void reverse(long time) throws InterruptedException {
+        drive.drive(-1, 0, 0);
+        try {
+            sleep(time);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        drive.drive(0, 0, 0);
+    }
+    public void rotate(long time, int direction) throws InterruptedException {
+        drive.drive(0, 0, direction);
+        try {
+            sleep(time);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        drive.drive(0, 0, 0);
+    }
+    public void strafe(long time, int direction){
+        drive.drive(0, direction, 0);
+        try {
+            sleep(time);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        drive.drive(0, 0, 0);
+    }
+    @Override
+    public void init() {
+        drive = new MecanumDriveTrain(hardwareMap);
+        telemetry.addLine("V1");
+        telemetry.addLine("Place robot on small triangle, facing forward");
+        telemetry.update();
+    }
+
+
+    @Override
+    public void start() {
+        strafe(step1, -1);
+    }
+    @Override
+    public void loop() {
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto3.java
@@ -0,0 +1,74 @@
+package org.firstinspires.ftc.teamcode;
+
+import static java.lang.Thread.sleep;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.mechanisms.MecanumDriveTrain;
+
+@Autonomous(name = "Back up, strafe to the right, shoot, then strafe off line", group = "Blue Auto")
+public class Auto3 extends OpMode {
+    int step1 = 2000;
+    int step2 = 1000;
+    int step3 = 2000;
+    private MecanumDriveTrain drive;
+    private Launch_v1 launch;
+    public void forward(long time){
+        drive.drive(1, 0, 0);
+        try {
+            sleep(time);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        drive.drive(0, 0, 0);
+    }
+    public void reverse(long time){
+        drive.drive(-1, 0, 0);
+        try {
+            sleep(time);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        drive.drive(0, 0, 0);
+    }
+    public void rotate(long time, int direction) {
+        drive.drive(0, 0, direction);
+        try {
+            sleep(time);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        drive.drive(0, 0, 0);
+    }
+    public void strafe(long time, int direction){
+        drive.drive(0, direction, 0);
+        try {
+            sleep(time);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        drive.drive(0, 0, 0);
+    }
+    @Override
+    public void init() {
+        drive = new MecanumDriveTrain(hardwareMap);
+        launch = new Launch_v1(hardwareMap);
+        telemetry.addLine("V1");
+        telemetry.addLine("Place robot facing against the left edge of the goal");
+        telemetry.update();
+    }
+
+
+    @Override
+    public void start() {
+        reverse(step1);
+        strafe(step2, 1);
+        launch.run(true);
+        strafe(step3, -1);
+    }
+    @Override
+    public void loop() {
+
+    }
+}
