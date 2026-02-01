171fc7858543c328182156cd92029fd8c0a97758
Reaper754 <MasonT019@stemcollegiate.ca>
2026-02-01 12:44:44 -0700

    Update SimpleMecanumTeleOp.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SimpleMecanumTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SimpleMecanumTeleOp.java
@@ -8,7 +8,7 @@ import org.firstinspires.ftc.teamcode.mechanisms.ArcadeDrivetrain;
 import org.firstinspires.ftc.teamcode.mechanisms.Intake;
 import org.firstinspires.ftc.teamcode.mechanisms.MecanumDriveTrain;
 import org.firstinspires.ftc.teamcode.mechanisms.SpinSorter;
-import org.firstinspires.ftc.teamcode.mechanisms.Hood;
+//import org.firstinspires.ftc.teamcode.mechanisms.Hood;
 import org.firstinspires.ftc.teamcode.Launch_v1;
 import org.firstinspires.ftc.teamcode.mechanisms.Feeder;
 import org.firstinspires.ftc.teamcode.mechanisms.Flywheel;
@@ -17,7 +17,7 @@ public class SimpleMecanumTeleOp extends OpMode {
     MecanumDriveTrain drivetrain = null;
     Intake intake = null;
     SpinSorter sorter = null;
-    Hood hood = null;
+    //Hood hood = null;
     Launch_v1 launch = null;
     Feeder servoArm = null;
     Flywheel flywheel = null;
@@ -27,20 +27,20 @@ public class SimpleMecanumTeleOp extends OpMode {
         drivetrain = new MecanumDriveTrain(hardwareMap);
         intake = new Intake(hardwareMap);
         sorter = new SpinSorter(hardwareMap, 0.5);
-        hood = new Hood(hardwareMap, 0);
+        //hood = new Hood(hardwareMap, 0);
         launch = new Launch_v1(hardwareMap);
         servoArm = new Feeder(hardwareMap);
         flywheel = new Flywheel(hardwareMap, REVERSE, FORWARD);
         drivetrain.init(REVERSE, FORWARD, REVERSE, FORWARD);
         intake.init(FORWARD);
         drivetrain.driveFieldRelative(0, 0, 0);
-        telemetry.addLine("V45");
+        telemetry.addLine("V46");
     }
 
     @Override
     public void loop() {
         double forward = gamepad1.left_stick_y;
-        double right = gamepad1.left_stick_x;
+        double right = -gamepad1.left_stick_x;
         double rotate = -gamepad1.right_stick_x;
         drivetrain.driveFieldRelative(forward, right, rotate);
         if (gamepad1.a) {
@@ -80,18 +80,18 @@ public class SimpleMecanumTeleOp extends OpMode {
             sorter.SpinRight(false);
             sorter.init();
         }
-        if (gamepad1.dpadLeftWasPressed()) {
-            hood.hood_down(true);
-        }
-        if (gamepad2.dpadRightWasPressed()) {
-            hood.hood_up(true);
-        }
-        if (gamepad2.dpadLeftWasReleased()) {
-            hood.hood_down(false);
-        }
-        if (gamepad2.dpadRightWasReleased()) {
-            hood.hood_up(false);
-        }
+        //if (gamepad1.dpadLeftWasPressed()) {
+            //hood.hood_down(true);
+        //}
+        //if (gamepad2.dpadRightWasPressed()) {
+            //hood.hood_up(true);
+        ///}
+        //if (gamepad2.dpadLeftWasReleased()) {
+            //hood.hood_down(false);
+        //}
+        //if (gamepad2.dpadRightWasReleased()) {
+            //hood.hood_up(false);
+        //}
         //if (gamepad2.rightBumperWasPressed()) {
             //launch.run(true);
         //}
@@ -123,7 +123,7 @@ public class SimpleMecanumTeleOp extends OpMode {
         telemetry.addData("Minimum Velocity: ", flywheel.min_velocity);
         telemetry.addData("Velocity of Flywheel Motor 1: ", flywheel.launcher.getVelocity());
         telemetry.addData("Velocity of Flywheel Motor 2: ", flywheel.launcherTwo.getVelocity());
-        telemetry.addData("Hood Position: ", hood.hood_position);
+        //telemetry.addData("Hood Position: ", hood.hood_position);
         flywheel.run();
 
     }
