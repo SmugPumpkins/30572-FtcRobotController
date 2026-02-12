20fb1f1f9fe4075b57361f4cdaaf53178d9995ed
borgle-johnson <theohutchinson6@gmail.com>
2026-02-12 15:39:49 -0600

    i was gonna write 'autosort works now. don't ask how', but it doesn't work so

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/ColourSensor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/ColourSensor.java
@@ -11,20 +11,28 @@ public class ColourSensor {
     public enum colour {
         GREEN,
         PURPLE,
+        NONE,
         UNKNOWN
     }
     public void init(HardwareMap hardwareMap) {
         colourSensor = hardwareMap.get(NormalizedColorSensor.class, "colourSensor");
+        colourSensor.setGain(16);
     }
-    public colour getColour(Telemetry telemetry) {
+    public colour getColour() {
         NormalizedRGBA colors = colourSensor.getNormalizedColors(); // return 4 values
         float normRed, normGreen, normBlue;
         normRed = colors.red/colors.alpha;
         normGreen = colors.green/colors.alpha;
         normBlue = colors.blue/colors.alpha;
-        telemetry.addData("Red", normRed);
-        telemetry.addData("Green", normGreen);
-        telemetry.addData("Blue", normBlue);
+        if(normRed < 0.3 && normGreen < 0.32 && normBlue < 0.25) {
+            return colour.NONE;
+        }
+        else if(normGreen > 0.33) {
+            return colour.GREEN;
+        }
+        else if(normGreen > 0.22 && normRed < 0.23 && normGreen < 0.33) {
+            return colour.PURPLE;
+        }
         return colour.UNKNOWN;
     }
 }
